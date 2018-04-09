768b1a4f556bbb167f577f8a080c9a44e3a24288
Yanilda <yanilda.peralta@gmail.com>
2018-04-09 12:08:04 -0400

    Something

--- app/app.py
+++ app/app.py
@@ -105,5 +105,5 @@ def userHome():
 if __name__ == "__main__":
     print("Running on port 5000")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=port)
+    app.run(host='0.0.0.0', port=port)
     #0.0.0.0
